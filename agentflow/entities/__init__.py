5c3ec37f3892b4e2cd9a06190e2ae4652f77c332
AliiiBenn <davidddpereiraaa6@gmail.com>
2026-01-16 10:34:22 +0100

    chore: export all entity classes from entities module
    Update entities/__init__.py to export:
    - Action
    - Commit
    - Session
    - SessionStatus
    - Workspace
    
    Allows importing entities directly from agentflow.entities

--- agentflow/entities/__init__.py
+++ agentflow/entities/__init__.py
@@ -1,1 +1,14 @@
-"""Entity models module."""
+"""Entity models for AgentFlow."""
+
+from agentflow.entities.action import Action
+from agentflow.entities.commit import Commit
+from agentflow.entities.session import Session, SessionStatus
+from agentflow.entities.workspace import Workspace
+
+__all__ = [
+    "Action",
+    "Commit",
+    "Session",
+    "SessionStatus",
+    "Workspace",
+]
